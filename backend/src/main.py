5e20fecf2d305abab83b30ae91d7f2771a938d55
March7040 <148756009+March7040@users.noreply.github.com>
2026-05-08 00:58:23 +0300

    Update main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, status
-from fastapi.middleware.cors import CORSMiddleware # ДОДАНО: імпорт CORS
+from fastapi.middleware.cors import CORSMiddleware
 from bson import ObjectId
-from database.database import users_collection, campaigns_collection, transactions_collection
+from database import users_collection, campaigns_collection, transactions_collection
 from models import User, Campaign, Transaction
 from typing import List
 from passlib.context import CryptContext
@@ -24,6 +24,8 @@ async def root():
     return {"message": "Сервер працює."}
 
 #КОРИСТУВАЧІ
+
+# Створити користувача
 @app.post("/users/")
 async def create_user(user: User):
     safe_password = user.hashed_password[:72]
@@ -37,9 +39,30 @@ async def create_user(user: User):
         "user_id": str(new_user.inserted_id)
     }
 
+# Отримати всі збори конкретного волонтера
+@app.get("/users/{user_id}/campaigns", response_model=List[Campaign])
+async def get_user_campaigns(user_id: str):
+    campaigns = []
+    cursor = campaigns_collection.find({"volunteer_id": user_id})
+    async for document in cursor:
+        campaigns.append(Campaign(**document))
+    return campaigns
+
+# Отримати історію донатів користувача
+@app.get("/users/{user_email}/donations", response_model=List[Transaction])
+async def get_user_donations(user_email: str):
+    transactions = []
+    cursor = transactions_collection.find({
+        "donor_email": user_email,
+        "status": "success"
+    })
+    async for document in cursor:
+        transactions.append(Transaction(**document))
+    return transactions
+
 #ЗБОРИ
 
-# 1. Створення нового збору
+# Створення нового збору
 @app.post("/campaigns/")
 async def create_campaign(campaign: Campaign):
     user = await users_collection.find_one({"_id": ObjectId(campaign.volunteer_id)})
@@ -47,14 +70,16 @@ async def create_campaign(campaign: Campaign):
     if not user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, 
-            detail="Користувача з таким ID не знайдено"
+            detail="Користувача не знайдено"
         )
-        
-    if not user.get("is_verified"):
+    
+    if user.get("verification_status") != "verified":
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, 
-            detail="Помилка: Створювати збори можуть лише верифіковані волонтери."
+            detail="Помилка: Створювати збори можуть лише верифіковані волонтери"
         )
+    campaign.author_name = user.get("full_name", "Невідомий")
+    campaign.is_author_verified = True
 
     campaign_dict = campaign.model_dump(by_alias=True, exclude={"id"})
     new_campaign = await campaigns_collection.insert_one(campaign_dict)
@@ -64,22 +89,22 @@ async def create_campaign(campaign: Campaign):
         "campaign_id": str(new_campaign.inserted_id)
     }
 
-# 2. Отримання списку всіх зборів
+# Отримання списку всіх зборів на головному екрані
 @app.get("/campaigns/", response_model=List[Campaign])
-async def get_all_campaigns():
+async def get_all_campaigns(limit: int = 6):
     campaigns = []
-    cursor = campaigns_collection.find()
+    cursor = campaigns_collection.find({"status": "active"}).limit(limit)
     
     async for document in cursor:
         campaigns.append(Campaign(**document))
         
     return campaigns
+
+# Процес донату
 @app.post("/donate/")
 async def create_donation(transaction: Transaction):
     transaction_dict = transaction.model_dump(by_alias=True, exclude={"id"})
-    
     new_transaction = await transactions_collection.insert_one(transaction_dict)
-    
     fake_payment_url = f"https://api.monobank.ua/checkout/pay/{new_transaction.inserted_id}"
     
     return {
@@ -91,7 +116,7 @@ async def create_donation(transaction: Transaction):
 
 #ВЕРИФІКАЦІЯ ТА УПРАВЛІННЯ
 
-# 1. Верифікація волонтера
+# Верифікація волонтера
 @app.put("/users/{user_id}/verify")
 async def verify_user(user_id: str):
     result = await users_collection.update_one(
@@ -103,7 +128,7 @@ async def verify_user(user_id: str):
         return {"message": "Користувача успішно верифіковано."}
     return {"error": "Користувача не знайдено або він вже верифікований."}
 
-# 2. Закриття збору
+# Закриття збору
 @app.put("/campaigns/{campaign_id}/close")
 async def close_campaign(campaign_id: str):
     result = await campaigns_collection.update_one(
@@ -115,7 +140,7 @@ async def close_campaign(campaign_id: str):
         return {"message": "Збір успішно закрито!"}
     return {"error": "Збір не знайдено."}
 
-# 3. Фінал донату
+# Фінал донату
 @app.post("/webhook/payment/{transaction_id}")
 async def payment_webhook(transaction_id: str):
     transaction = await transactions_collection.find_one({"_id": ObjectId(transaction_id)})
